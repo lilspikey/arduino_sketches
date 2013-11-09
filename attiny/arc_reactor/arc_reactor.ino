e7417e112e464897add31a49155a3908f3aff8d2
John Montgomery <john@littlespikeyland.com>
2013-11-09 08:10:54 +0000

    Got simple cycle animation of "arc reactor" working
    Powers up, stays on for five seconds, then powers down again in reverse
    sequence - all after button is pressed.

--- attiny/arc_reactor/arc_reactor.ino
+++ attiny/arc_reactor/arc_reactor.ino
@@ -9,17 +9,27 @@
 
 #ifdef __AVR_ATtiny85__
 #define SWITCH_PIN 0
-#define LED_PIN 1
+#define LED1_PIN 2
+#define LED2_PIN 3
+#define LED3_PIN 4
 #else
 #define SWITCH_PIN 2 // (interrupt 0)
 #define LED_PIN 3
 #endif
 
+#define CYCLES 8
+
+prog_uint16_t cycle_delays[CYCLES] PROGMEM = {
+  750, 500, 250, 200, 150, 50, 10, 10
+};
+
 void setup() {
   pinMode(SWITCH_PIN, INPUT);
   digitalWrite(SWITCH_PIN, HIGH);
   
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+  pinMode(LED3_PIN, OUTPUT);
   
   ADCSRA &= ~(1<<ADEN);                     //turn off ADC
   ACSR |= _BV(ACD);                         //disable the analog comparator
@@ -48,15 +58,41 @@ void gotoSleep() {
 #endif
 }
 
+void cycle(int delayMillis) {
+  for ( int pin = LED1_PIN; pin <= LED3_PIN; pin++ ) {
+    digitalWrite(LED1_PIN, pin == LED1_PIN? HIGH : LOW);
+    digitalWrite(LED2_PIN, pin == LED2_PIN? HIGH : LOW);
+    digitalWrite(LED3_PIN, pin == LED3_PIN? HIGH : LOW);
+    delay(delayMillis);
+  }
+}
+
+void all(uint8_t state) {
+  digitalWrite(LED1_PIN, state);
+  digitalWrite(LED2_PIN, state);
+  digitalWrite(LED3_PIN, state);
+}
+
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  
   gotoSleep();
   
   // Wait for the button to be released
   while (digitalRead(SWITCH_PIN) == LOW) {  }
+  
+  for ( int i = 0; i < CYCLES; i++ ) {
+    int delayMillis = 0xFFFF & pgm_read_word_near(cycle_delays + i);
+    cycle(delayMillis);
+  }
+  
+  all(HIGH);
+  delay(5000);
+  
+  for ( int i = CYCLES-1; i >= 0; i-- ) {
+    int delayMillis = 0xFFFF & pgm_read_word_near(cycle_delays + i);
+    cycle(delayMillis);
+  }
+  
+  all(LOW);
 }
 
 void interrup() {}
